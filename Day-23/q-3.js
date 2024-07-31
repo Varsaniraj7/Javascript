bf21e8c9671da880f050c35c77cd1b9b303bb144
Varsaniraj7 <varsaniraj7@gmail.com>
2024-07-31 15:34:49 +0530

    commited

--- Day-23/q-3.js
+++ Day-23/q-3.js
@@ -1,1 +1,9 @@
 // Write a simple number guessing game. Provide a secret number and a guess. Based on those numbers give players clues if their guess is higher, lower or correct.
+let val = 15;
+if(val> 20){
+    console.log("Your guess is too high");
+}else if(val <=10){
+    console.log("Your guess is too low");
+}else{
+    console.log("Congratulations! Your guess is correct");
+}

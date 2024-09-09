d355930aa52e05c6d7a94a0b1f3a5dbd0949a8e6
Varsaniraj7 <varsaniraj7@gmail.com>
2024-09-09 15:54:00 +0530

    commited

---
+++ loop.js
@@ -0,0 +1,23 @@
+// for loop
+for(let i = 0 ; i<=10 ;i++){
+    console.log(i);
+}
+
+// while
+let i=0;
+while(i<=10){
+    console.log(i);
+    i++
+}
+
+// do while
+do{
+    console.log(i);
+    i++;
+}while(i<10);
+
+// foreach
+let arr = [10,20,30,40,50]
+arr.forEach((i)=>{
+    console.log(i);
+})
